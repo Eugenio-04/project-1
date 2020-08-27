11467577004be5a2c1399ec9db74d299e5ace8c0
Eugenio-04 <70342510+Eugenio-04@users.noreply.github.com>
2020-08-27 13:24:35 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+basic.showNumber(9)
+basic.clearScreen()
+basic.showLeds(`
+    # # # . .
+    # . . . .
+    # # # . .
+    # . . . .
+    # # # . .
+    `)
+basic.forever(function () {
+	
+})
